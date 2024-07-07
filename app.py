b2b6e5509839dbfd09cb19df38d640c425aa1e9d
Matheus Ferreira <matheusfdantas03@gmail.com>
2024-07-07 19:25:07 -0300

    Implementing a Payment Creation Route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payment import Payment
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -10,7 +11,20 @@ db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_pix_payment():
-    return jsonify({'message': 'Pix payment created!'}), 201
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({'message': 'Value is required!'}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({'message': 'Pix payment created!',
+                    'payment': new_payment.to_dict()}), 201
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def confirm_pix_payment():
